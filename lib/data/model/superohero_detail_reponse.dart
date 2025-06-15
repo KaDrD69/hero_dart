f9303fe0e8299ba35ebe725bba3b65d13ab55b2c
KaDrD <kadrd@localhost.localdomain>
2025-06-15 18:31:36 -0300

    finalizado, pero faltan detalles

--- lib/data/model/superohero_detail_reponse.dart
+++ lib/data/model/superohero_detail_reponse.dart
@@ -3,12 +3,14 @@ class SuperoheroDetailReponse {
   final String name;
   final String url;
   final String realName;
+  final PowerstatsResponse powerStatsResponse;
 
   SuperoheroDetailReponse({
     required this.id,
     required this.name,
     required this.url,
     required this.realName,
+    required this.powerStatsResponse,
   });
 
   factory SuperoheroDetailReponse.fromJson(Map<String, dynamic> json) {
@@ -17,6 +19,36 @@ class SuperoheroDetailReponse {
       name: json["name"],
       url: json["image"]["url"],
       realName: json["biography"]["full-name"],
+      powerStatsResponse: PowerstatsResponse.fromJson(json["powerstats"]),
     );
   }
+}
+
+class PowerstatsResponse {
+  final String intelligence;
+  final String strength;
+  final String speed;
+  final String durability;
+  final String power;
+  final String combat;
+
+  PowerstatsResponse({
+    required this.intelligence,
+    required this.strength,
+    required this.speed,
+    required this.durability,
+    required this.power,
+    required this.combat,
+  });
+
+  factory PowerstatsResponse.fromJson(Map<String, dynamic> json){
+    return PowerstatsResponse(
+      intelligence: json["intelligence"],
+      strength: json["strength"],
+      speed: json["speed"],
+      durability: json["durability"],
+      power: json["power"],
+      combat: json["combat"]);
+  }
+
 }
--- lib/screens/superhero_detail_screen.dart
+++ lib/screens/superhero_detail_screen.dart
@@ -12,12 +12,67 @@ class SuperheroDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(superhero.name),
       ),
-      body: Column(
-        children: [
-          Image.network(superhero.url, height: 300, width: double.infinity, fit: BoxFit.cover, alignment: Alignment(0, -0.6),),
-          Text(superhero.name, style: TextStyle(fontSize: 28),),
-          Text(superhero.realName, style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic))
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(superhero.url, height: 300, width: double.infinity, fit: BoxFit.cover, alignment: Alignment(0, -0.6),),
+            Text(superhero.name, style: TextStyle(fontSize: 28),),
+            Text(superhero.realName, style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic)),
+           // Text(superhero.powerStatsResponse.combat, style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic)),
+            SizedBox(
+              height: 130,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(height: (double.tryParse(superhero.powerStatsResponse.power) ?? 0).clamp(0, 100), width: 20, color: Colors.red,),
+                      Text("Power")
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(height: (double.tryParse(superhero.powerStatsResponse.intelligence) ?? 0).clamp(0, 100), width: 20, color: Colors.blue,),
+                      Text("intelligence")
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(height: (double.tryParse(superhero.powerStatsResponse.strength) ?? 0).clamp(0, 100), width: 20, color: Colors.grey,),
+                      Text("Strength")
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(height: (double.tryParse(superhero.powerStatsResponse.speed) ?? 0).clamp(0, 100), width: 20, color: Colors.green,),
+                      Text("speed")
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(height: (double.tryParse(superhero.powerStatsResponse.durability) ?? 0).clamp(0, 100), width: 20, color: Colors.orange,),
+                      Text("Durability")
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(height: (double.tryParse(superhero.powerStatsResponse.combat) ?? 0).clamp(0, 100), width: 20, color: Colors.black,),
+                      Text("Combat")
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
